a3a4775c0db73f19db645fc5795dac708c7c36c9
hieuvnautotech <hieuvnautotech@gmail.com>
2023-09-07 13:11:43 +0700

    ok

--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -3,6 +3,9 @@ import { Button, Form, Checkbox, Segment } from "semantic-ui-react";
 import ButtonOrCancel from './ButtonOrCancel'
 import EntryForm from './EntryForm';
 
+// import useEntryDetails from '../hooks/useEntryDetails';
+
+
 function NewEntryForm({addEntry, isExpense, value, description, setDescription, setIsExpense, setValue}) {
     return (
       <Form unstackable>
